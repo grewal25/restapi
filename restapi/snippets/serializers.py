ae81c1bca1c65fc96eae5dc14a68f6b1d0df9ca4
Saranjeet Singh <grewalele@gmail.com>
2019-10-14 18:02:32 -0700

    url api working

--- restapi/snippets/serializers.py
+++ restapi/snippets/serializers.py
@@ -2,11 +2,17 @@ from rest_framework import serializers
 from snippets.models import Post, Choice
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
+    # choices=serializers.HyperlinkedRelatedField(
+    # many=True,
+    # read_only=True,
+    # view_name='post-list'
+    # )
     class Meta:
         model=Post
-        fields=['id','post_text']
+        fields=('url','pk','post_text')
 
 class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
+    post=serializers.SlugRelatedField(queryset=Post.objects.all(), slug_field='post_text')
     class Meta:
         model=Choice
-        fields=['id','choice_text','likes']
+        fields=('url','pk','choice_text','likes','post')
--- restapi/snippets/urls.py
+++ restapi/snippets/urls.py
@@ -3,8 +3,8 @@ from snippets import views
 
 urlpatterns=[
     path('post-list/', views.PostList.as_view(), name='post-list'),
-    path('post-list/<int:pk>/', views.PostDetail.as_view()),
+    path('post-list/<int:pk>/', views.PostDetail.as_view(),name='post-detail'),
     path('choice-list/', views.ChoiceList.as_view(), name='choice-list'),
-    path('choice-list/<int:pk>/', views.ChoiceDetail.as_view()),
+    path('choice-list/<int:pk>/', views.ChoiceDetail.as_view(),name='choice-detail'),
     path('',views.ApiRoot.as_view(),name=views.ApiRoot.name),
 ]
